cdda07b4eb7d87ab366fc4b457b9c6b5785da655
George Magpie <prich845@gmail.com>
2020-06-27 12:28:13 +0300

    Попытка в тесты. Не удачная.

---
+++ chess/src/test/java/ru/job4j/chess/figures/black/TestBishopBlack.java
@@ -0,0 +1,28 @@
+package ru.job4j.chess.figures.black;
+
+import org.junit.Test;
+import ru.job4j.chess.firuges.black.BishopBlack;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+public class TestBishopBlack {
+
+    @Test
+    public void testPosition(){
+        BishopBlack bishop = new BishopBlack();
+        bishop.position();
+        assertThat();
+    }
+
+    @Test
+    public void testCopy(){
+
+    }
+
+    @Test
+    public void testWay(){
+
+    }
+
+}
